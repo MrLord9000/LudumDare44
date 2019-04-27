9c535c6f750b62dba71b5cb8182f77e306bee9c7
witoldm <witoldm@protonmail.ch>
2019-04-27 19:18:19 +0200

    Ugly clock, tourbar, coin display.

---
+++ LD44_project/Assets/Scripts/UI/Coin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class Coin : MonoBehaviour
+{
+    private TextMeshProUGUI textMeshPro;
+    private int coins;
+    private void Awake()
+    {
+        textMeshPro = GetComponent<TextMeshProUGUI>();
+        coins = 0;
+    }
+
+    void Update()
+    {
+        textMeshPro.SetText(coins.ToString());
+        if (Input.GetKeyDown("c"))
+        {
+            coins++;
+        }
+    }
+}
