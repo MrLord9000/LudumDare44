efbd85ddc83818f5bece1b546fcc189c43d45eba
witoldm <witoldm@protonmail.ch>
2019-04-28 04:52:57 +0200

    Primitive menu, audio mixer.

---
+++ LD44_project/Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public void NewGame()
+    {
+         UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LaunchLevel1()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");//level1
+    }
+    public void LaunchLevel2()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");//level2
+    }
+    public void LaunchLevel3()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");//level3
+    }
+}
